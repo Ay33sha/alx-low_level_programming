e81015fa86c4ce00738ce2a377e3e4e6a352f475
Aisha Nasir <nasircutieeesha@gmail.com>
2022-06-03 18:42:56 -0700

    C program followed by a new line Using the function puts program should end with the value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int main (void)
+{
+char str1[60];
+
+
+strcpy(str1, "\"Programming is like building a multilingual puzzle,");
+
+puts(str1);
+
+return(0);
+}
